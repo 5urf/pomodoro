7e9b4bd4cadb898741799a0ed69fa73b6f90d554
LeeYunHak <sza0203@naver.com>
2025-03-28 23:32:59 +0900

    style(theme): modalBgColor 추가

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,6 +3,7 @@ import "styled-components";
 declare module "styled-components" {
   export interface DefaultTheme {
     bgColor: React.CSSProperties["backgroundColor"];
+    modalBgColor: React.CSSProperties["backgroundColor"];
     textColor: React.CSSProperties["color"];
     accentColor: React.CSSProperties["color"];
     subTextColor: React.CSSProperties["color"];
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,7 @@ import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
   bgColor: "#c8d6dc",
+  modalBgColor: "#f0e2cd",
   textColor: "#4c525d",
   accentColor: "#f8dcc9",
   subTextColor: "#f5f7f7 ",
